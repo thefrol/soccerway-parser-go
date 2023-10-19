3e21506a02a99e75b2e7368a3c588bf29b283bf0
Dima Frolenko <mail@frolenkodima.ru>
2023-10-19 15:52:45 +0300

    feat: Добавить парсинг чисел  и дат

--- internal/parse/parser.go
+++ internal/parse/parser.go
@@ -3,7 +3,13 @@
 // из HTML данные и преобразовывать их
 package parse
 
-import "github.com/gocolly/colly"
+import (
+	"log"
+	"strconv"
+	"time"
+
+	"github.com/gocolly/colly"
+)
 
 // Text достает текстовое значение тега, указанного в tag, и записывает
 // в переменную, на которую ссылкается out. Данные беруться из коллектора c
@@ -14,5 +20,29 @@ import "github.com/gocolly/colly"
 func Text(c *colly.Collector, tag string, out *string) {
 	c.OnHTML(tag, func(e *colly.HTMLElement) {
 		*out = e.Text
+	})
+}
+
+// Int достает переменную типа int из тега tag. Работает
+// как Text(), только преобразует строку в число
+func Int(c *colly.Collector, tag string, out *int) {
+	c.OnHTML(tag, func(e *colly.HTMLElement) {
+		i, err := strconv.ParseInt(e.Text, 10, 64)
+		if err != nil {
+			log.Printf("cant parse %v as int", tag)
+		}
+		*out = int(i)
+	})
+}
+
+// Date достает переменную типа time.Time из тега tag. Работает
+// как Text(), только преобразует строку в число
+func Date(c *colly.Collector, tag string, out *time.Time) {
+	c.OnHTML(tag, func(e *colly.HTMLElement) {
+		t, err := time.Parse("2 January 2006", e.Text)
+		if err != nil {
+			log.Printf("cant parse %v as time", tag)
+		}
+		*out = t
 	})
 }
--- internal/player/raw.go
+++ internal/player/raw.go
@@ -3,13 +3,15 @@
 // из страницы
 package player
 
+import "time"
+
 // Raw содержит чистые строки, как были получены из HTML
 type Raw struct {
 	FirstName      string
 	LastName       string
 	Nationality    string
-	DateOfBirth    string
-	Age            string
+	DateOfBirth    time.Time
+	Age            int
 	CountryOfBirth string
 	Position       string
 	Height         string
