ec64b6d3280df1f371c3c93d1a2686cdaefe8ed9
Dima Frolenko <mail@frolenkodima.ru>
2023-10-19 15:25:47 +0300

    feat: выделить парсер для текста в строку

--- cmd/function/main.go
+++ cmd/function/main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/gocolly/colly"
+	"github.com/thefrol/soccerway-parser-go/internal/parse"
 )
 
 // Hanler это точка входа для веб-контейнера типа aws-lambda,
@@ -20,18 +21,19 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	c := colly.NewCollector(colly.UserAgent("Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/117.0"))
 
-	// Find and visit all links
-	c.OnHTML("dd[data-first_name]", func(e *colly.HTMLElement) {
-		fmt.Println(e.Text)
-	})
-
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
 	})
+
+	var firstName, lastName string
+	parse.Text(c, "dd[data-first_name]", &firstName)
+	parse.Text(c, "dd[data-last_name]", &lastName)
 
 	err := c.Visit("http://us.soccerway.com/players/danilo-malik-santos/871010/")
 	if err != nil {
 		// todo.. if forbidden
 		log.Fatal(err)
 	}
+
+	fmt.Println(firstName, lastName)
 }
